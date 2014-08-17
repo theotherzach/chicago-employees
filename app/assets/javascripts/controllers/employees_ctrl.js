84ead31cc6cdc011c75ba7f20550848e5e3816ee
Zach <briggszj@gmail.com>
2014-08-17 10:45:59 -0500

    Bootstrap employees data on load.

--- app/assets/javascripts/controllers/employees_ctrl.js
+++ app/assets/javascripts/controllers/employees_ctrl.js
@@ -32,6 +32,10 @@
   angular.module("app").controller("employeesCtrl", function ($scope, $http) {
     $scope.page = 1;
 
+    $scope.init = function (stuff) {
+      $scope.employees = stuff.employees;
+    }
+
     $http.get("/api/employees").then(function (response) {
       $scope.employees = response.data;
     });
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,5 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.all.limit(10).to_json
   end
 end
